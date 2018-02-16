ce315952fcd163a2dffd24a5cfb040251eb6e194
vagnerpraia <vagnerpraia@gmail.com>
2018-02-16 12:54:40 -0200

    Adicionando configurações do projeto

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,10 @@ module.exports = {
                         ]
                     }
                 )
+            },
+            {
+                test: /\.woff|\.woff2\.ttf\.eot\.svg*.*$/,
+                loader: 'file'
             }
         ]
     }
